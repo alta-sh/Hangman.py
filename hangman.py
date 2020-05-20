fb3176346872eec53a2af6a5e461c67f89cd6b70
Alex Ashley <me@alta.sh>
2020-05-20 03:23:56 +0100

    created game states & exit condition

--- hangman.py
+++ hangman.py
@@ -1,9 +1,17 @@
 # A hangman bot for discord written by alta
+import hangState
 import discord
 from discord.ext import commands
 
+runningGames = []
+
 client = commands.Bot(command_prefix='hang!')
 client.remove_command('help')
+
+def isInGame(authorID):
+    if (authorID in runningGames):
+        return True
+    return False
 
 @client.event
 async def on_ready():
@@ -11,11 +19,23 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    if (isInGame(message.author.id)):
+
+        if (message.content == "end"):
+            await message.channel.send("Thanks for playing {0}!\nGame successfully ended...".format(message.author.mention))
+            runningGames.remove(message.author.id)
+        else:
+            await message.channel.send("You're in a game...")
+
+
     await client.process_commands(message)
 
 @client.command()
 async def play(ctx):
     await ctx.send('Ok {0}, setting up the game now 👍'.format(ctx.message.author.mention))
+    # The user id of the current player
+    runningGames.append(ctx.message.author.id)
+
 
 # Reading the token
 with open('token.txt', 'r') as file:
