1994160b98ac231c1f03dcd84b40cdc0a43e0278
Alex Ashley <me@alta.sh>
2020-05-20 14:37:11 +0100

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -3,15 +3,22 @@ import hangState
 import discord
 from discord.ext import commands
 
-runningGames = []
-
 client = commands.Bot(command_prefix='hang!')
 client.remove_command('help')
 
+runningGames = []
+commands = ["help", "play"]
+
 def isInGame(authorID):
-    if (authorID in runningGames):
-        return True
+    for game in runningGames:
+        if (game["author"] == authorID):
+            return True
     return False
+
+def endGame(authorID):
+    for game in runningGames:
+        if (game["author"] == authorID):
+            del runningGames[game]
 
 @client.event
 async def on_ready():
@@ -19,22 +26,39 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+
+    # Check if the message sender is currently in a game:
     if (isInGame(message.author.id)):
 
+        # check if they've sent a command
+        if (message.content.startswith(client.command_prefix)):
+            await message.channel.send(f"You can't use that command whilst in a game {message.author.name}...\n"+
+                                        "If you would like to end the game type `end` 🤗")
+            return
+
+        # If they end the game
         if (message.content == "end"):
             await message.channel.send("Thanks for playing {0}!\nGame successfully ended...".format(message.author.mention))
-            runningGames.remove(message.author.id)
+            endGame(message.author.id)
         else:
-            await message.channel.send("You're in a game...")
-
+            # process the message 
+            await message.channel.send("You're in a game")
 
     await client.process_commands(message)
+
+
 
 @client.command()
 async def play(ctx):
     await ctx.send('Ok {0}, setting up the game now 👍'.format(ctx.message.author.mention))
-    # The user id of the current player
-    runningGames.append(ctx.message.author.id)
+
+    # Check if user isn't already in a game
+    if(isInGame(ctx.message.author.id)):
+        await ctx.send(f'Actually {ctx.message.author.name}, it looks like you\'re already in a game...' +
+                        'type `end` to leave it.')
+    else:
+        # Add userID and guesses to runningGames
+        runningGames.append({"author": ctx.message.author.id, "remainingGuesses": 0})
 
 
 # Reading the token
