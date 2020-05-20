06e1b5f1a086ce46b7c6498c4d46d8e217d91c42
Alex Ashley <me@alta.sh>
2020-05-20 15:52:53 +0100

    message handling when in game & error guards

--- hangman.py
+++ hangman.py
@@ -9,44 +9,60 @@ client.remove_command('help')
 runningGames = []
 commands = ["help", "play"]
 
+# Checks if the user is in a game (if the author.id is in runningGames)
 def isInGame(authorID):
     for game in runningGames:
         if (game["author"] == authorID):
             return True
     return False
 
+# Deletes the authors dict in runningGames
 def endGame(authorID):
     for game in runningGames:
         if (game["author"] == authorID):
-            del runningGames[game]
+            del runningGames[runningGames.index(game)]
+
 
 @client.event
 async def on_ready():
     print('Successfully logged in as {0.user}'.format(client))
 
+
 @client.event
 async def on_message(message):
+
+    message.content = message.content.lower()
 
     # Check if the message sender is currently in a game:
     if (isInGame(message.author.id)):
 
-        # check if they've sent a command
-        if (message.content.startswith(client.command_prefix)):
-            await message.channel.send(f"You can't use that command whilst in a game {message.author.name}...\n"+
-                                        "If you would like to end the game type `end` 🤗")
-            return
+        if (message.content != "hang!guess"):
+            # check if they've sent a command
+            if (message.content.startswith(client.command_prefix)):
+                await message.channel.send(f"You can't use that command whilst in a game {message.author.name}...\n"+
+                                            "If you would like to end the game type `end` 🤗")
+                return
 
-        # If they end the game
-        if (message.content == "end"):
-            await message.channel.send("Thanks for playing {0}!\nGame successfully ended...".format(message.author.mention))
-            endGame(message.author.id)
-        else:
-            # process the message 
-            await message.channel.send("You're in a game")
+            # If they end the game
+            if (message.content == "end"):
+                await message.channel.send(f"Thanks for playing {message.author.mention}!\nGame successfully ended...")
+                endGame(message.author.id)
+
+            # If they enter a word without writing hang!guess first
+            elif (len(message.content) > 1):
+                await message.channel.send(f"{message.author.mention} please only enter 1 character at a time!\n"+
+                                            "If you would like to guess the word use the command \n`hang!guess {your guess}`\n"+
+                                            "Or if you would like to quit the game write `end`")
+
+            else: # process the message as a character guess 
+                await message.channel.send("You guessed a character...")
 
     await client.process_commands(message)
 
 
+@client.command()
+async def help(ctx):
+    await ctx.send('here are the commands you can use:... ')
 
 @client.command()
 async def play(ctx):
@@ -57,8 +73,9 @@ async def play(ctx):
         await ctx.send(f'Actually {ctx.message.author.name}, it looks like you\'re already in a game...' +
                         'type `end` to leave it.')
     else:
-        # Add userID and guesses to runningGames
-        runningGames.append({"author": ctx.message.author.id, "remainingGuesses": 0})
+        # Add userID, guesses and a random word to runningGames
+        randWord = ''
+        runningGames.append({"author": ctx.message.author.id, "remainingGuesses": 0, "word": randWord})
 
 
 # Reading the token
